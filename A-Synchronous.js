d01e53831dacbe815e39c36096ed0cc880bdc4dd
Devendra Solanki <devendrasolanki1485@gmail.com>
2025-12-30 10:55:16 +0530

    feat: implement promise-based asynchronous behavior in A-Synchronous.js

--- A-Synchronous.js
+++ A-Synchronous.js
@@ -11,3 +11,21 @@
 //     b = 20;
 // },2000);
 // console.log("sum is ",(a+b));
+
+
+//promiss
+
+
+console.log("Start")
+var a =10;
+let b = 0;
+let waitingData = new Promise( (resolve , reject)=>{
+setTimeout(()=>{
+    resolve(20);
+},1000)
+})
+waitingData.then((data)=>{
+    b=data;
+    console.log("sum is ",(a+b));
+}).catch(()=>{console.log("some error is there")})
+console.log("Done")
