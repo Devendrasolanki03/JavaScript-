9e3c0bf6bb3ef68145537cb5be659594437eb0cf
Devendra Solanki <devendrasolanki1485@gmail.com>
2025-12-30 11:48:01 +0530

    refactor: remove commented-out code for getData function and clean up promise chaining example in A-Synchronous.js

--- A-Synchronous.js
+++ A-Synchronous.js
@@ -48,7 +48,6 @@
 
 
 // 03
-
 // function getData(id){
 //     setTimeout(()=>{
 //         console.log(" data ",id)
@@ -76,34 +75,36 @@
 
 
 // 05 promise chaining
+// function getData(id) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data", id);
+//             resolve("success");
+//         }, 2000);
+//     });
+// }
+// console.log("waiting for data 1");
+// getData(1)
+//     .then((result) => {
+//         console.log(result);
+//         console.log("waiting for data 2");
+//         return getData(2);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         console.log("waiting for data 3");
+//         return getData(3);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         console.log("all data received");
+//     })
+//     .catch((error) => {
+//         console.log("error:", error);
+//     });
 
-function getData(id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data", id);
-            resolve("success");
-        }, 2000);
-    });
-}
-console.log("waiting for data 1");
-getData(1)
-    .then((result) => {
-        console.log(result);
-        console.log("waiting for data 2");
-        return getData(2);
-    })
-    .then((result) => {
-        console.log(result);
-        console.log("waiting for data 3");
-        return getData(3);
-    })
-    .then((result) => {
-        console.log(result);
-        console.log("all data received");
-    })
-    .catch((error) => {
-        console.log("error:", error);
-    });
-    
+
+
+
 
 
